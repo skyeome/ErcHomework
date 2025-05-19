799dc75464e390625fc6b8f6dcff03540dccb37a
skyeome <tjru7230@naver.com>
2025-05-19 17:39:04 +0900

    ui: reading/new 작업중

--- app/(app)/reading/new.tsx
+++ app/(app)/reading/new.tsx
@@ -1,11 +1,38 @@
-import { View, Text } from "react-native";
+import { Button, ButtonText } from "@/components/ui/button";
+import { FormControl } from "@/components/ui/form-control";
+import { Heading } from "@/components/ui/heading";
+import { Input, InputField } from "@/components/ui/input";
+import { Text } from "@/components/ui/text";
+import { VStack } from "@/components/ui/vstack";
 import React from "react";
 
 const New = () => {
   return (
-    <View>
-      <Text>New</Text>
-    </View>
+    <FormControl className="p-4">
+      <VStack space="xl">
+        <VStack space="xs">
+          <Heading className="pb-5 text-typography-900">
+            Enter Your Homework Information
+          </Heading>
+          <Text className="text-typography-500">Title</Text>
+          <Input className="min-w-[250px]">
+            <InputField type="text" />
+          </Input>
+        </VStack>
+        <VStack space="xs">
+          <Button className="w-full" variant="outline">
+            <ButtonText className="text-typography-900">
+              Upload Again
+            </ButtonText>
+          </Button>
+          <Button className="w-full">
+            <ButtonText className="text-typography-0">
+              Submit Homework
+            </ButtonText>
+          </Button>
+        </VStack>
+      </VStack>
+    </FormControl>
   );
 };
 
